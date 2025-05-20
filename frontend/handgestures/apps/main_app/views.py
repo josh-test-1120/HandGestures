674e1b375239d1a5cebf6c3f00f5fffd16c7c5a5
speedrun-program <71526906+speedrun-program@users.noreply.github.com>
2025-05-20 01:34:59 -0700

    Add files via upload

--- frontend/handgestures/apps/main_app/views.py
+++ frontend/handgestures/apps/main_app/views.py
@@ -1,4 +1,7 @@
 
+import mysql.connector
+from mysql.connector import errorcode
+
 # These are the most commonly used elements for application views
 from django.shortcuts import render, redirect
 from django.contrib import messages
@@ -10,9 +13,6 @@ from django.views.decorators.csrf import csrf_exempt
 import json
 
 import random
-
-import mysql.connector
-from mysql.connector import errorcode
 
 
 CONFIG = {
@@ -79,149 +79,109 @@ def contact(request):
 
 def summary_page_update(request):
     query_string = """
-        SELECT COUNT(DISTINCT participant),
-            COUNT(*),
-            (SELECT COUNT(*) FROM handdata WHERE label IS NOT NULL AND prediction IS NOT NULL),
-            (SELECT COUNT(*) FROM handdata WHERE label = prediction)
-        FROM handdata;
+        SELECT ParticipantCount,
+            RowCount,
+            CorrectCount
+        FROM RunningData;
     """
     
-    participants = None
-    data_points = None
-    prediction_accuracy = None
-    
-    database_read_successfully = False
+    participants = 0
+    data_points = 0
+    prediction_accuracy = 0.0
     
     if cursor is not None:
         cursor.execute(query_string)
         result = next(iter(cursor.fetchall()), None)
         if result is not None:
             
-            database_read_successfully = True
-            
             participants = result[0]
             data_points = result[1]
-            if result[2] != 0:
-                prediction_accuracy = (result[3] / result[2]) * 100
+            if data_points != 0:
+                prediction_accuracy = (result[2] / data_points) * 100
             else:
                 prediction_accuracy = 0.0
     
-    if database_read_successfully:
-        summary_data = {
-            "participants": "{:,}".format(participants),
-            "data_points": "{:,}".format(data_points),
-            "prediction_accuracy": "{:,.2f}".format(prediction_accuracy),
-        }
-    else:
-        summary_data = {
-            "participants": "error",
-            "data_points": "error",
-            "prediction_accuracy": "error",
-        }
+    summary_data = {
+        "participants": "{:,}".format(participants),
+        "data_points": "{:,}".format(data_points),
+        "prediction_accuracy": "{:,.2f}".format(prediction_accuracy),
+    }
     
     return JsonResponse(summary_data)
 
 
 def demo_page_update(request):
-    query_string = """
-        WITH cte AS (
-            SELECT *,
-                SQRT(POW(accelx, 2) + POW(accely, 2) + POW(accelz, 2)) AS total_accel,
-                SQRT(POW(gyrox, 2) + POW(gyroy, 2) + POW(gyroz, 2)) AS total_rotation
-            FROM handdata
-        )
-        SELECT AVG(accelx),
-            AVG(accely),
-            AVG(accelz),
-            AVG(gyrox),
-            AVG(gyroy),
-            AVG(gyroz),
-            (SELECT total_accel FROM cte ORDER BY total_accel DESC LIMIT 0, 1) AS max_accel_1,
-            (SELECT total_accel FROM cte ORDER BY total_accel DESC LIMIT 1, 1) AS max_accel_2,
-            (SELECT total_accel FROM cte ORDER BY total_accel DESC LIMIT 2, 1) AS max_accel_3,
-            (SELECT total_rotation FROM cte ORDER BY total_rotation DESC LIMIT 0, 1) AS max_rotation_1,
-            (SELECT total_rotation FROM cte ORDER BY total_rotation DESC LIMIT 1, 1) AS max_rotation_2,
-            (SELECT total_rotation FROM cte ORDER BY total_rotation DESC LIMIT 2, 1) AS max_rotation_3
-        FROM cte;
+    query_string_top_accel = """
+        SELECT TotalAccel
+        FROM TotalSpeeds
+        ORDER BY TotalAccel DESC LIMIT 3
     """
     
-    avg_accel_x = None
-    avg_accel_y = None
-    avg_accel_z = None
-    avg_rotation_x = None
-    avg_rotation_y = None
-    avg_rotation_z = None
-    max_accel_1 = None
-    max_accel_2 = None
-    max_accel_3 = None
-    max_rotation_1 = None
-    max_rotation_2 = None
-    max_rotation_3 = None
+    query_string_top_rotation = """
+        SELECT TotalRotation
+        FROM TotalSpeeds
+        ORDER BY TotalRotation DESC LIMIT 3
+    """
     
-    database_read_successfully = False
+    query_string_averages = """
+        SELECT AverageXAccel,
+            AverageYAccel,
+            AverageZAccel,
+            AverageXRotation,
+            AverageYRotation,
+            AverageZRotation
+        FROM RunningData
+    """
+    
+    avg_accel_x = 0.0
+    avg_accel_y = 0.0
+    avg_accel_z = 0.0
+    avg_rotation_x = 0.0
+    avg_rotation_y = 0.0
+    avg_rotation_z = 0.0
+    max_accel = [0.0, 0.0, 0.0]
+    max_rotation = [0.0, 0.0, 0.0]
     
     if cursor is not None:
-        cursor.execute(query_string)
+        
+        cursor.execute(query_string_top_accel)
+        iter_results = iter(cursor.fetchall())
+        max_accel = [next(iter_results, (0.0,))[0] for _ in range(len(max_accel))]
+        
+        cursor.execute(query_string_top_rotation)
+        iter_results = iter(cursor.fetchall())
+        max_rotation = [next(iter_results, (0.0,))[0] for _ in range(len(max_rotation))]
+        
+        cursor.execute(query_string_averages)
         result = next(iter(cursor.fetchall()), None)
         if result is not None:
-            
-            database_read_successfully = True
-            
             avg_accel_x = result[0]
             avg_accel_y = result[1]
             avg_accel_z = result[2]
             avg_rotation_x = result[3]
             avg_rotation_y = result[4]
             avg_rotation_z = result[5]
-            max_accel_1 = result[6]
-            max_accel_2 = result[7]
-            max_accel_3 = result[8]
-            max_rotation_1 = result[9]
-            max_rotation_2 = result[10]
-            max_rotation_3 = result[11]
     
-    if database_read_successfully:
-        demo_data = {
-            "avg_x_accel": "average x acceleration: {:,.3f}".format(avg_accel_x),
-            "avg_y_accel": "average y acceleration: {:,.3f}".format(avg_accel_y),
-            "avg_z_accel": "average z acceleration: {:,.3f}".format(avg_accel_z),
-            "avg_x_rot": "average x rotation: {:,.3f}".format(avg_rotation_x),
-            "avg_y_rot": "average y rotation: {:,.3f}".format(avg_rotation_y),
-            "avg_z_rot": "average z rotation: {:,.3f}".format(avg_rotation_z),
-            "first_fastest_accel": "1st fastest acceleration outlier: {:,.3f}".format(max_accel_1),
-            "first_fastest_accel_explanation": "Explanation: n/a",
-            "second_fastest_accel": "2nd fastest acceleration outlier: {:,.3f}".format(max_accel_2),
-            "second_fastest_accel_explanation": "Explanation: n/a",
-            "third_fastest_accel": "3rd fastest acceleration outlier: {:,.3f}".format(max_accel_3),
-            "third_fastest_accel_explanation": "Explanation: n/a",
-            "first_fastest_rot": "1st fastest rotation outlier: {:,.3f}".format(max_rotation_1),
-            "first_fastest_rot_explanation": "Explanation: n/a",
-            "second_fastest_rot": "2nd fastest rotation outlier: {:,.3f}".format(max_rotation_2),
-            "second_fastest_rot_explanation": "Explanation: n/a",
-            "third_fastest_rot": "3rd fastest rotation outlier: {:,.3f}".format(max_rotation_3),
-            "third_fastest_rot_explanation": "Explanation: n/a",
-        }
-    else:
-        demo_data = {
-            "avg_x_accel": "error",
-            "avg_y_accel": "error",
-            "avg_z_accel": "error",
-            "avg_x_rot": "error",
-            "avg_y_rot": "error",
-            "avg_z_rot": "error",
-            "first_fastest_accel": "error",
-            "first_fastest_accel_explanation": "error",
-            "second_fastest_accel": "error",
-            "second_fastest_accel_explanation": "error",
-            "third_fastest_accel": "error",
-            "third_fastest_accel_explanation": "error",
-            "first_fastest_rot": "error",
-            "first_fastest_rot_explanation": "error",
-            "second_fastest_rot": "error",
-            "second_fastest_rot_explanation": "error",
-            "third_fastest_rot": "error",
-            "third_fastest_rot_explanation": "error",
-        }
+    demo_data = {
+        "avg_x_accel": "average x acceleration: {:,.3f}".format(avg_accel_x),
+        "avg_y_accel": "average y acceleration: {:,.3f}".format(avg_accel_y),
+        "avg_z_accel": "average z acceleration: {:,.3f}".format(avg_accel_z),
+        "avg_x_rot": "average x rotation: {:,.3f}".format(avg_rotation_x),
+        "avg_y_rot": "average y rotation: {:,.3f}".format(avg_rotation_y),
+        "avg_z_rot": "average z rotation: {:,.3f}".format(avg_rotation_z),
+        "first_fastest_accel": "1st fastest acceleration outlier: {:,.3f}".format(max_accel[0]),
+        "first_fastest_accel_explanation": "Explanation: n/a",
+        "second_fastest_accel": "2nd fastest acceleration outlier: {:,.3f}".format(max_accel[1]),
+        "second_fastest_accel_explanation": "Explanation: n/a",
+        "third_fastest_accel": "3rd fastest acceleration outlier: {:,.3f}".format(max_accel[2]),
+        "third_fastest_accel_explanation": "Explanation: n/a",
+        "first_fastest_rot": "1st fastest rotation outlier: {:,.3f}".format(max_rotation[0]),
+        "first_fastest_rot_explanation": "Explanation: n/a",
+        "second_fastest_rot": "2nd fastest rotation outlier: {:,.3f}".format(max_rotation[1]),
+        "second_fastest_rot_explanation": "Explanation: n/a",
+        "third_fastest_rot": "3rd fastest rotation outlier: {:,.3f}".format(max_rotation[2]),
+        "third_fastest_rot_explanation": "Explanation: n/a",
+    }
     
     return JsonResponse(demo_data)
 
